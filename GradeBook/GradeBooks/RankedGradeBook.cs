971c3fc6586bc8d928b6bc80a8589da108be8c79
joe-hennequin <63567558+joe-hennequin@users.noreply.github.com>
2020-05-14 18:14:53 +1000

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,7 +36,7 @@
             if (Students.Count < 5)
                 System.Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
             else
-                base.CalculateStudentStatistics();
+                base.CalculateStudentStatistics(name);
         }
     }
 }
